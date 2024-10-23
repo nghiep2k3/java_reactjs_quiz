357f50c7b4adf96e1171857959e15757a230172e
Nguyễn Nghiệp <nghiep2k3>
2024-10-24 00:31:54 +0700

    code

--- FE/src/index.js
+++ FE/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import ViewQuiz from "./pages/ViewQuiz/ViewQuiz";
+import Result from './pages/result/result.jsx';
 import CreateQuiz from "./pages/createQuiz/createQuiz";
 import CreateQuestion from "./pages/createQuestion/createQuestion";
 import MyLibrary from "./components/myLibrary/myLibrary";
@@ -17,47 +18,43 @@ import DoExam from "./pages/doExam/doExam";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Result from "./pages/result/result.jsx";
 import ReportQuizResult from "./pages/reportQuizResult/reportQuizResult";
 import ChangePassword from "./pages/changePassword/changePassword";
 import Profile from "./pages/profile/profile";
 import Test from "./pages/Test/Test.jsx";
-import { LoginWrapper } from "./components/context/login.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LoginWrapper>
-      <Router future={{ v7_startTransition: true }}>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="/mylibrary" element={<MyLibrary />} />
-            <Route path="/ViewQuiz" element={<ViewQuiz />} />
-            <Route path="/" element={<Explore />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="/reportquizresult" element={<ReportQuizResult />} />
-            <Route path="/test" element={<Test />} />
-            <Route path="/createquiz" element={<CreateQuiz />}>
-              <Route path="inforquiz" element={<InforQuiz />} />
-              <Route path="createquestion" element={<CreateQuestion />} />
-            </Route>
-            <Route path="/quizlist" element={<QuizList />} />
-            <Route path="/profile" element={<Profile />} />
+    <Router future={{ v7_startTransition: true }}>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/" element={<App />}>
+          <Route path="/mylibrary" element={<MyLibrary />} />
+          <Route path="/ViewQuiz" element={<ViewQuiz />} />
+          <Route path="/" element={<Explore />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/reportquizresult" element={<ReportQuizResult />} />
+          <Route path="/createquiz" element={<CreateQuiz />}>
+            <Route path="inforquiz" element={<InforQuiz />} />
+            <Route path="createquestion" element={<CreateQuestion />} />
           </Route>
-        </Routes>
+          <Route path="/quizlist" element={<QuizList />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
 
-        <Routes>
-          <Route path="/quizdetail" element={<QuizDetail />} >
-            <Route path="examcontent/:id" element={<ExamContent />} />
-          </Route>
-          <Route path="/doexam/:id" element={<DoExam />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/changepass" element={<ChangePassword />} />
-        </Routes>
-      </Router>
-    </LoginWrapper>
+      <Routes>
+        <Route path="/quizdetail" element={<QuizDetail />} >
+          <Route path="examcontent/:id" element={<ExamContent />} />
+        </Route>
+        <Route path="/doexam/:id" element={<DoExam />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/changepass" element={<ChangePassword />} />
+      </Routes>
+    </Router>
   </React.StrictMode >
 );
 
