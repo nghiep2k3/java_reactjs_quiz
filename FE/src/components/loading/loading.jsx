6c9e0ee0e0142313421962c149bcee3fdb62b80c
Nguyễn Thiện Nghiệp <114827693+nghiep2k3@users.noreply.github.com>
2024-10-27 16:21:53 +0700

    Delete FE directory

--- FE/src/components/loading/loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spin } from 'antd';
-const Loading = () => <Spin style={{
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-}} />;
-export default Loading;
